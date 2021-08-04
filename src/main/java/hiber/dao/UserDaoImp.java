1f33b24451cc2fb467d9fefbad1647a54ec351bf
taq368 <84571379+taq368@users.noreply.github.com>
2021-08-04 12:17:39 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,38 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User get(Car car) {
+      Query query = sessionFactory.getCurrentSession().createQuery("from User where car = :car_id");
+      query.setParameter("car_id", car);
+      return (User)query.getSingleResult();
+   }
+
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,61 @@
+package hiber.model;
+
+import hiber.model.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "series")
+    private int series;
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(String name, int series) {
+        this.name = name;
+        this.series = series;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
